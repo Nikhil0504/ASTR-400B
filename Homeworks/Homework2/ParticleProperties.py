a0af70f8f5fe631f999e693378985601f3917cb7
Nikhil0504 <nikhilgaruda@live.com>
2025-01-26 00:25:59 -0700

    Add HW 2 files (need to maybe update docs)

---
+++ Homeworks/Homework2/ParticleProperties.py
@@ -0,0 +1,33 @@
+from ReadFile import Read
+
+import numpy as np
+import astropy.units as u
+
+def ParticleInfo(filename, part_type, part_num):
+    _, _, data = Read(filename)
+
+    # Extracting the data for the given particle type
+    index = np.where(data['type'] == part_type)
+    data = data[index]
+
+    # Extracting the data for the given particle number
+    part_info = data[part_num]
+
+    # Calculating the distance, velocity, and mass of the particle
+    dist = np.sqrt(part_info['x']**2 + part_info['y']**2 + part_info['z']**2) * u.kpc
+    vel = np.sqrt(part_info['vx']**2 + part_info['vy']**2 + part_info['vz']**2) * (u.km/u.s)
+    mass = part_info['m'] * 1e10 * u.M_sun
+
+    # round the values to 3 decimal places
+    dist = np.round(dist, 3)
+    vel = np.round(vel, 3)
+
+    return dist, vel, mass
+
+if __name__ == '__main__':
+    # Testing the function
+    dist, vel, mass = ParticleInfo('../../MW_000.txt', 2, 100-1)
+    print('Distance:', dist)
+    print('Distance in lyr:', dist.to(u.lyr).round(3))
+    print('Velocity:', vel)
+    print('Mass:', mass)
---
+++ Homeworks/Homework2/ReadFile.py
@@ -0,0 +1,34 @@
+import numpy as np
+import astropy.units as u
+
+def Read(filename):
+    # open the file
+    file = open(filename,'r')
+
+    # read the first line
+    line1 = file.readline()
+    label, value = line1.split()
+    time = float(value) * u.Myr
+
+    # read the second line
+    line2 = file.readline()
+    label, value = line2.split()
+    total_particles = int(value)
+
+    # close the file
+    file.close()
+
+    # read the data
+    data = np.genfromtxt(filename, dtype=None, names=True, skip_header=3)
+
+    return time, total_particles, data
+
+
+if __name__ == '__main__':
+    # Testing the function
+    time, total_particles, data = Read('../../MW_000.txt')
+    print('Time:', time)
+    print('Total Particles:', total_particles)
+    
+    # Test with second particle
+    print('Particle 2:', data['type'][1], data['m'][1], data['x'][1], data['y'][1], data['z'][1], data['vx'][1], data['vy'][1], data['vz'][1])
